f13172283f352deb2d8e391bbe85b368b15989f6
Xumoyiddin014 <xtuxtamirzayev@gmail.com>
2025-02-15 12:11:37 +0500

    add docker and docker-compose.

--- Maxwaypractic/settings.py
+++ Maxwaypractic/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^-&x9=flgvlw1hxx%&wmruzqfh2qj9_7nyxwb@76mv&i2$g#u@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
